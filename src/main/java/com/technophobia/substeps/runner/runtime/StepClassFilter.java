9dbfd244eb99a52571501e059942bd7fcbcafdcf
Stu Forbes <mr.stu.forbes@gmail.com>
2012-07-23 16:37:19 +0100

    Stu: ClassAnalyser can now be passed into SyntaxBuilder; StepClassFilter and StepClassLocator now support execution with a different classloader

--- src/main/java/com/technophobia/substeps/runner/runtime/StepClassFilter.java
+++ src/main/java/com/technophobia/substeps/runner/runtime/StepClassFilter.java
@@ -1,5 +1,6 @@
 package com.technophobia.substeps.runner.runtime;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 import com.google.common.base.Predicate;
@@ -7,13 +8,40 @@ import com.technophobia.substeps.model.SubSteps;
 
 public class StepClassFilter implements Predicate<Class<?>> {
 
-	public boolean apply(final Class<?> clazz) {
-		for (final Method method : clazz.getMethods()) {
-			if (method.isAnnotationPresent(SubSteps.Step.class)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public boolean apply(final Class<?> clazz) {
+        if (hasAnnotation(clazz, SubSteps.AdditionalStepImplementations.class)) {
+            return true;
+        }
+        for (final Method method : clazz.getMethods()) {
+            if (hasAnnotation(method, SubSteps.Step.class)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private boolean hasAnnotation(final Method method, final Class<?> annotationClass) {
+        return hasAnnotation(method.getAnnotations(), annotationClass);
+    }
+
+
+    private boolean hasAnnotation(final Class<?> clazz, final Class<?> annotationClass) {
+        return hasAnnotation(clazz.getAnnotations(), annotationClass);
+    }
+
+
+    private boolean hasAnnotation(final Annotation[] annotations, final Class<?> annotationClass) {
+        // compare the class names rather than the classes themselves, as its
+        // entirely possible that the class was created in a different
+        // classloader to
+        // the current classs (e.g. if this is the eclipse plugin)
+        for (final Annotation annotation : annotations) {
+            if (annotation.annotationType().getName().equals(annotationClass.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
--- src/main/java/com/technophobia/substeps/runner/runtime/StepClassLocator.java
+++ src/main/java/com/technophobia/substeps/runner/runtime/StepClassLocator.java
@@ -5,4 +5,8 @@ public class StepClassLocator extends PredicatedClassLocator {
     public StepClassLocator(final String path) {
         super(new StepClassFilter(), new ClassLoadingFunction(path));
     }
+    
+    public StepClassLocator(final String path, ClassLoader classLoader){
+    	super(new StepClassFilter(), new ClassLoadingFunction(classLoader, path));
+    }
 }
