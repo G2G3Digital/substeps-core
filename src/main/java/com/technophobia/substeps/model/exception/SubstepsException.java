5b9e49f40d592d69a4924bec792d0b3bb52ae548
David Moss <david.moss@technophobia.com>
2012-11-21 12:00:31 +0000

    Remove java 7 constructor.

--- src/main/java/com/technophobia/substeps/model/exception/SubstepsException.java
+++ src/main/java/com/technophobia/substeps/model/exception/SubstepsException.java
@@ -8,11 +8,6 @@ public class SubstepsException extends RuntimeException {
         super();
     }
 
-    public SubstepsException(final String message, final Throwable cause, final boolean enableSuppression,
-            final boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
-
     public SubstepsException(final String message, final Throwable cause) {
         super(message, cause);
     }
